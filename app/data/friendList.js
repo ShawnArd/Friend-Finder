1724d167f5341711c5584ff63cf9c652dd2435a2
Shawn Ard <shawnduncanard@gmail.com>
2018-12-23 17:25:27 -0600

    base

---
+++ app/data/friendList.js
@@ -0,0 +1,26 @@
+var friend = [
+    {
+        name:"Marth",
+        img: "",
+        score: [1,1,1,1,1,1,1,1,1,1]
+    },
+    {
+        name:"Kirby",
+        img: "",
+        score: [2,2,2,2,2,2,2,2,2,2]  
+    },
+    {
+        name:"Mario",
+        img: "",
+        score: [4,4,4,4,4,4,4,4,4,4]  
+    },
+    {
+        name:"Bayonetta",
+        img: "",
+        score: [5,5,5,5,5,5,5,5,5,5]  
+    },
+];
+
+module.exports = friend;
+
+
