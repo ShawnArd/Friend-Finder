bc7b4aad16d9161c4bc347d7041c22659f479fe2
Shawn Ard <shawnduncanard@gmail.com>
2018-12-23 21:15:12 -0600

    updated

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
+
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, './app/public')));
 
-var PORT = process.env.PORT || 3000;
 
-require("./app/routes/apiRoutes")(app);
-require("./app/routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function() {
