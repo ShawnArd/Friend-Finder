d78ff29a539d7e2e4c943c7b1246742a1f480101
Shawn Ard <shawnduncanard@gmail.com>
2018-12-23 21:41:11 -0600

    added sql

---
+++ app/data/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO characters (name, photo, scores) VALUES ("Kirby", "https://upload.wikimedia.org/wikipedia/en/5/5c/Kirby.png", "[1,1,1,1,1,1,1,1,1,1]");
+
+INSERT INTO characters (name, photo, scores) VALUES ("Mario", "https://en.wikipedia.org/wiki/File:MarioSMBW.png#/media/File:MarioSMBW.png", "[3,3,3,3,3,3,3,3,3,3]");
+
+INSERT INTO characters (name, photo, scores) VALUES ("Wario", "https://en.wikipedia.org/wiki/Wario#/media/File:Wario-mario_party_8.png", "[4,4,4,4,4,4,4,4,4,4]");
+
+INSERT INTO characters (name, photo, scores) VALUES ("Waluigi", "https://en.wikipedia.org/wiki/Waluigi#/media/File:Waluigi.png", "[5,5,5,5,5,5,5,5,5,5,5]");
